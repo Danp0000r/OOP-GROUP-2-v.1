156849ebfc3f5f1fefaab9506f93368078c12bea
Danrich <danrich.hernandez19@email.com>
2026-06-03 15:29:06 +0800

    mobile view

--- run.py
+++ run.py
@@ -4,6 +4,6 @@ from __init__ import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    debug_mode = os.environ.get('FLASK_DEBUG', 'true').lower() == 'true'
+    debug_mode = os.environ.get('FLASK_DEBUG', 'false').lower() == 'true'
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=debug_mode, host="0.0.0.0", port=port)
